4e58e58bca87daa11b319d6e9bde8c55bfec7309
Allan Gabriel <allan.gabriel@protonmail.com>
2020-09-22 15:06:27 -0300

    create routes

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("working");
-});
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+//routes
+const indexRoute = require(__dirname + "/routes");
+
+app.use("/", indexRoute);
 
 app.listen(process.env.PORT, () => {
     console.log(`running at ${process.env.PORT}`);
