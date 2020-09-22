8631a19b8a0b27e8bcc45a91587ddaa78ce5785a
Allan Gabriel <allan.gabriel@protonmail.com>
2020-09-22 18:10:51 -0300

    add created timestamp

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
     title: String,
-    content: String
+    content: String,
+    created: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 module.exports = mongoose.model("post", PostSchema);
