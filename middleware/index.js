3fd4bfc0e14636aa3fd1996dc32c207137cb69ab
Allan Gabriel <allan.gabriel@protonmail.com>
2020-09-29 23:30:00 -0300

    control access middlewares

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,19 @@
 const User = require("../models/User");
 
+module.exports.isAuthenticated = (req, res, next) => {
+    if(req.isAuthenticated())
+        next();
+    else 
+        res.redirect("back");
+};
+
+module.exports.notAuthenticated = (req, res, next) => {
+    if (!req.isAuthenticated())
+        next();
+    else
+        res.redirect("back");
+};
+
 module.exports.registerPermissions = (req, res, next) => {
     User.find({ isAdmin: true }, (err, foundAdmin) => {
         if (!foundAdmin.length > 0 || req.user && req.user.isAdmin) {
